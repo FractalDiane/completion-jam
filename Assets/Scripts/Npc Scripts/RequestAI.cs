15307f64e943be1af9bb4a4dda24e0d99119bba9
Victor Zhou <44218140+Fireryrex@users.noreply.github.com>
2020-12-27 23:24:16 -0800

    timer bar and billboarding compelte

--- Assets/Scripts/Npc Scripts/RequestAI.cs
+++ Assets/Scripts/Npc Scripts/RequestAI.cs
@@ -1,24 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+public enum ItemType { Medicine, Toys, GetSeated };
 
 public class RequestAI : MonoBehaviour
 {
-    [SerializeField] TimeMeter timerLogic;
+    [SerializeField] bool timeIsReducing = false;
+    [SerializeField] ItemType requiredObject;
+
+    [SerializeField] float maxRequestGap = 30;
+    [SerializeField] float currentRequestGap = 30;
+    [SerializeField] bool gapActive = false;
+    [SerializeField] ItemType[] requestsList;
+    [SerializeField] Slider timeSlider;
+    [SerializeField] Gradient timeGradient;
+    [SerializeField] Image timeFill;
     // Start is called before the first frame update
     void Start()
     {
-        timerLogic = gameObject.GetComponent<TimeMeter>();
+        SetMaxSatisfaction(5f, ItemType.GetSeated);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            CheckRequirement(ItemType.GetSeated);
+        }
+        if (timeIsReducing)
+        {
+            timeSlider.value -= Time.deltaTime;
+            timeFill.color = timeGradient.Evaluate(timeSlider.value / timeSlider.maxValue);
+            if (timeSlider.value <= 0)
+            {
+                timeIsReducing = false;
+                // call timer fail
+                Debug.Log("Timer Failed 1");
+            }
+        }
+
+        if (gapActive)
+        {
+            currentRequestGap -= Time.deltaTime;
+            if(currentRequestGap <= 0)
+            {
+                gapActive = false;
+                //select new request
+                SetMaxSatisfaction(30f, requestsList[Random.Range(0, System.Enum.GetValues(typeof(ItemType)).Length - 1)]);
+            }
+        }
     }
 
-    public void CreateRequest()
+    public void SetMaxSatisfaction(float max, ItemType obj)
     {
+        timeSlider.maxValue = max;
+        timeSlider.value = timeSlider.maxValue;
+        timeFill.color = timeGradient.Evaluate(1f);
+        timeIsReducing = true;
+        requiredObject = obj;
+    }
 
+    public void CheckRequirement(ItemType obj)
+    {
+        if (requiredObject == obj)
+        {
+            timeSlider.maxValue = 0;
+            timeSlider.value = 0;
+            timeIsReducing = false;
+            currentRequestGap = maxRequestGap;
+            gapActive = true;
+        }
+        else
+        {
+            Debug.Log("Timer Failed 2");
+        }
     }
 }
