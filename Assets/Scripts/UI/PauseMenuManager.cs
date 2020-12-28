47fa08b6bb6abedd8f55e813fa7003884b5b16ae
Weiyan Zhu <weiyanz@berkeley.edu>
2020-12-28 14:32:53 -0800

    Pause Menu is here

--- Assets/Scripts/UI/PauseMenuManager.cs
+++ Assets/Scripts/UI/PauseMenuManager.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject pauseMenuPage;
+    [SerializeField] private GameObject mainMenuConfimPage;
+    [SerializeField] private GameObject exitConfirmPage;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Pause"))
+        {
+            if(pauseMenuPage.activeSelf)
+                Resume();
+            else
+                pauseMenuPage.SetActive(!pauseMenuPage.activeSelf);
+        }
+    }
+
+    void ShowPaueMenu()
+    {
+        pauseMenuPage.SetActive(true);
+        mainMenuConfimPage.SetActive(false);
+        exitConfirmPage.SetActive(false);
+    }
+
+    public void Resume()
+    {
+        pauseMenuPage.SetActive(false);
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void CancelConfirmation()
+    {
+        mainMenuConfimPage.SetActive(false);
+        exitConfirmPage.SetActive(false);
+    }
+
+    public void ShowMainMenuConfirmation()
+    {
+        mainMenuConfimPage.SetActive(true);
+    }
+
+    public void ShowExitConfirmation()
+    {
+        exitConfirmPage.SetActive(true);
     }
 }
