964d0a68d56f5f68d66d27e4d642555f49c91824
Weiyan Zhu <weiyanz@berkeley.edu>
2020-12-29 15:33:09 -0800

    Modify Game Over win page to be able to go to next level

--- Assets/Scripts/UI/GameOverMenuManager.cs
+++ Assets/Scripts/UI/GameOverMenuManager.cs
@@ -5,8 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class GameOverMenuManager : UIManager
 {
-    [SerializeField] private GameObject winPage;
     [SerializeField] private GameObject losePage;
+    [SerializeField] private GameObject winPage;
+    [SerializeField] private bool hasNextLevel = true;
+    [SerializeField] private string nextLevelSceneName;
+    [SerializeField] private GameObject nextLevelButton;
+    [SerializeField] private GameObject mainMenuButton;
     public void Retry()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
@@ -26,5 +30,18 @@ public class GameOverMenuManager : UIManager
         Time.timeScale = 0;
         losePage.SetActive(false);
         winPage.SetActive(true);
+        if(hasNextLevel)
+        {
+            nextLevelButton.SetActive(true);
+            mainMenuButton.SetActive(false);
+        }else{
+            nextLevelButton.SetActive(false);
+            mainMenuButton.SetActive(true);
+        }
+    }
+
+    public void GoToNextLevel()
+    {
+        SceneManager.LoadScene(nextLevelSceneName);
     }
 }
